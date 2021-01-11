353b9802251bcb6613c6f6327dab3680bcecec54
Yasin Aksu <yasinaksu88@gmail.com>
2021-01-11 23:59:53 +0300

    EntityFramework implementation on core fraework layer

--- Northwind/Core/DataAccess/IEntityRepository.cs
+++ Northwind/Core/DataAccess/IEntityRepository.cs
@@ -11,7 +11,7 @@ namespace Core.DataAccess
     public interface IEntityRepository<T> where T : class, IEntity, new()
     {
         List<T> GetList(Expression<Func<T, bool>> filter = null);
-        T Get(Expression<Func<T>> filter);
+        T Get(Expression<Func<T, bool>> filter);
         T Add(T entity);
         T Update(T entity);
         void Delete(T entity);
