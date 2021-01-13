28b160279c918db3b4b201f8a82735a75ec8071b
Yasin Aksu <yasinaksu88@gmail.com>
2021-01-13 21:08:05 +0300

    NHibernate base implementation in Core layer

---
+++ Northwind/Core/DataAccess/NHibernate/NhQueryableRepository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.NHibernate
+{
+    public class NhQueryableRepository<T> : IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private readonly NHibernateHelper _nHibernateHelper;
+        private IQueryable<T> _entities;
+        public NhQueryableRepository(NHibernateHelper nHibernateHelper)
+        {
+            _nHibernateHelper = nHibernateHelper;
+        }
+
+        public IQueryable<T> Table => this.Entities;
+        protected virtual IQueryable<T> Entities => _entities ?? (_entities = _nHibernateHelper.OpenSession().Query<T>());
+    }
+}
