16b0e571dd64de1101712760ca5e134023a76b5c
Yasin Aksu <yasinaksu88@gmail.com>
2021-01-14 22:34:52 +0300

    At DataAccess layer entityframework configured and tested

---
+++ Northwind/Northwind.DataAccess.Tests/EntityFrameworkTests/EfProductDalTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Northwind.DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace Northwind.DataAccess.Tests.EntityFrameworkTests
+{
+    [TestClass]
+    public class EfProductDalTest
+    {
+        [TestMethod]
+        public void Get_list_returns_all_products()
+        {
+            EfProductDal productDal = new EfProductDal();
+            var products = productDal.GetList();
+            Assert.AreEqual(79, products.Count);
+        }
+
+        [TestMethod]
+        public void Get_list_with_parameters_returns_filtered_products()
+        {
+            EfProductDal productDal = new EfProductDal();
+            var products = productDal.GetList(x=>x.CategoryId==1);
+            Assert.AreEqual(14, products.Count);
+        }
+    }
+}
---
+++ Northwind/Northwind.DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,25 @@
+﻿using Northwind.DataAccess.Concrete.EntityFramework.Mappings;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DataAccess.Concrete.EntityFramework
+{
+    public class NorthwindContext : DbContext
+    {
+        public NorthwindContext()
+        {
+            Database.SetInitializer<NorthwindContext>(null);
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ProductMap());
+        }
+    }
+}
