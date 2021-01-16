d7e19c49dd680e27becc3fd4c8163d0de2c91f18
Yasin Aksu <yasinaksu88@gmail.com>
2021-01-16 12:39:57 +0300

    Business Layer implementations for product

---
+++ Northwind/Northwind.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,35 @@
+﻿using Northwind.Business.Abstract;
+using Northwind.DataAccess.Abstract;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.Concrete.Managers
+{
+    public class ProductManager : IProductService
+    {
+        private readonly IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+    }
+}
