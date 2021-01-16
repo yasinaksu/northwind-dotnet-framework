c662bc4d109cf5697ea3da6afba1fdfc29ff786b
Yasin Aksu <yasinaksu88@gmail.com>
2021-01-16 11:19:01 +0300

    Categories Table dataaccess processes added for entityframework

--- Northwind/Northwind.DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ Northwind/Northwind.DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -16,10 +16,12 @@ namespace Northwind.DataAccess.Concrete.EntityFramework
             Database.SetInitializer<NorthwindContext>(null);
         }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Configurations.Add(new ProductMap());
+            modelBuilder.Configurations.Add(new CategoryMap());
         }
     }
 }
---
+++ Northwind/Northwind.Entities/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Entities.Concrete
+{
+    public class Category : IEntity
+    {
+        public virtual int CategoryId { get; set; }
+        public virtual string CategoryName { get; set; }
+
+    }
+}
