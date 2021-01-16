0d0b5ba3b6e0f79dbc5217bef0eec0ebc31e3354
Yasin Aksu <yasinaksu88@gmail.com>
2021-01-16 11:30:46 +0300

    NhCategoryDal and EfCategoryDal tested and test is successful

---
+++ Northwind/Northwind.DataAccess.Tests/EntityFrameworkTests/EfCategoryDalTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Northwind.DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DataAccess.Tests.EntityFrameworkTests
+{
+    [TestClass]
+    public class EfCategoryDalTest
+    {
+        [TestMethod]
+        public void Get_list_returns_all_categories()
+        {
+            var categoryDal = new EfCategoryDal();
+            var result = categoryDal.GetList();
+            Assert.AreEqual(8, result.Count);
+        }
+    }
+}
---
+++ Northwind/Northwind.DataAccess.Tests/NHibernateTests/NhCategoryDalTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Northwind.DataAccess.Concrete.NHibernate;
+using Northwind.DataAccess.Concrete.NHibernate.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DataAccess.Tests.NHibernateTests
+{
+    [TestClass]
+    public class NhCategoryDalTest
+    {
+        [TestMethod]
+        public void Get_list_returns_all_categories()
+        {
+            var categoryDal = new NhCategoryDal(new SqlServerHelper());
+            var result = categoryDal.GetList();
+            Assert.AreEqual(8, result.Count);
+        }
+    }
+}
