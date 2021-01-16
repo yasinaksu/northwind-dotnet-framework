6f3b2dfd058d06330fa9722c7a48ce852e237a9c
Yasin Aksu <yasinaksu88@gmail.com>
2021-01-16 17:58:24 +0300

    Validation Aspect business tested and succed

---
+++ Northwind/Northwind.Business.Tests/ManagersTests/ProductManagerTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Northwind.Business.Concrete.Managers;
+using Northwind.DataAccess.Abstract;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.Tests.ManagersTests
+{
+    [TestClass]
+    public class ProductManagerTests
+    {
+        [ExpectedException(typeof(ValidationException))]
+        [TestMethod]
+        public void Add_fluent_validation_check()
+        {
+            var mock = new Mock<IProductDal>();
+            var productManager = new ProductManager(mock.Object);
+            productManager.Add(new Product());
+        }
+    }
+}
