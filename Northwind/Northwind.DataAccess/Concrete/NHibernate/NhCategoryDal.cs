d55ec66116dbf453c0b0d06569d14fa60233b909
Yasin Aksu <yasinaksu88@gmail.com>
2021-01-16 11:24:24 +0300

    Categories table nhibernate implementation in dataaccess

---
+++ Northwind/Northwind.DataAccess/Concrete/NHibernate/NhCategoryDal.cs
@@ -0,0 +1,18 @@
+﻿using Core.DataAccess.NHibernate;
+using Northwind.DataAccess.Abstract;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DataAccess.Concrete.NHibernate
+{
+    public class NhCategoryDal : NhEntityRepositoryBase<Category>, ICategoryDal
+    {
+        public NhCategoryDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
+        {
+        }
+    }
+}
