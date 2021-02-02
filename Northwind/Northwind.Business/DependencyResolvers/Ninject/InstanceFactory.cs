4b7facf13e39a3f826175e4eda5263a786de0ab8
Yasin Aksu <yasinaksu88@gmail.com>
2021-02-02 22:29:37 +0300

    instance factory

---
+++ Northwind/Northwind.Business/DependencyResolvers/Ninject/InstanceFactory.cs
@@ -0,0 +1,18 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.DependencyResolvers.Ninject
+{
+    public class InstanceFactory
+    {
+        public static T Get<T>()
+        {
+            var kernel = new StandardKernel(new BusinessModule());
+            return kernel.TryGet<T>();
+        }
+    }
+}
